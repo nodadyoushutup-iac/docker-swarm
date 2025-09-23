d1e5fa4434240f5deaa6289a361a1e3007fc8a37
nodadyoushutup <admin@nodadyoushutup.com>
2025-09-23 18:03:06 -0400

    move jenkins agent files

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 module "jenkins_controller" {
-  source = "./modules/jenkins-controller/terraform/app" # local use when repo is cloned to this location
+  source = "./jenkins-controller/terraform/module/app" # local use when repo is cloned to this location
   # source = "github.com/nodadyoushutup/jenkins-controller//app?ref=main"
 
   casc_config          = local.casc_config
   healthcheck_endpoint = format("%s/whoAmI/api/json?tree=authenticated", local.controller_url)
 }
 
-# module "jenkins_agent" {
-#   depends_on = [module.jenkins_controller]
-#   for_each   = { for node in local.casc_config.jenkins.nodes : node.permanent.name => node }
-#   source     = "./modules/jenkins-agent"
+module "jenkins_agent" {
+  depends_on = [module.jenkins_controller]
+  for_each   = { for node in local.casc_config.jenkins.nodes : node.permanent.name => node }
+  source     = "./jenkins-agent"
 
-#   name        = each.value.permanent.name
-#   jenkins_url = local.controller_url
-# }
+  name        = each.value.permanent.name
+  jenkins_url = local.controller_url
+}
