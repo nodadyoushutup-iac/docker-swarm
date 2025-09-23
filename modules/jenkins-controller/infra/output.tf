3e90f9dce24b1f3a25e660953a068e6fbbfb73aa
nodadyoushutup <admin@nodadyoushutup.com>
2025-09-23 16:29:17 -0400

    stop tracking nested repo files

--- modules/jenkins-controller/infra/output.tf
+++
@@ -1,9 +0,0 @@
-output "service_id" {
-  description = "ID of the Jenkins controller docker service"
-  value       = docker_service.controller.id
-}
-
-output "wait_for_service_id" {
-  description = "ID of the null resource waiting for the controller service"
-  value       = null_resource.wait_for_service.id
-}
